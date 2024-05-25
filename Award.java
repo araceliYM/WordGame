f2e32105f44f063af7de7cbd4f1da816b2bb0888
araceliYM <168685891+araceliYM@users.noreply.github.com>
2024-05-24 21:50:45 -0600

    Added Award.java via upload
    Award.java is a Java interface used to give prizes.

---
+++ Award.java
@@ -0,0 +1,8 @@
+// Purpose:       Java interface used to give prizes.
+// Programmer:    Araceli YM
+// Created on:    5/23/2024
+
+public interface Award
+{
+   public abstract int displayWinnings(Players winner, boolean result);
+}
