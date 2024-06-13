2befb025ded845c18c38be570c67222acffcef98
araceliYM <168685891+araceliYM@users.noreply.github.com>
2024-06-13 16:10:42 -0600

    Added Money.java via upload
    Updated output to be more consistent

---
+++ Money.java
@@ -0,0 +1,29 @@
+// Purpose:       Java program used to display the monetary winnings.
+// Programmer:    Araceli YM
+// Created on:    5/23/2024
+
+import javax.swing.JOptionPane;
+
+public class Money implements Award
+{
+   final double WIN_AMNT = 100.00;
+   final double LOSE_AMNT = 80.00;
+   
+   public int displayWinnings(Players player, boolean gameResult)
+   {
+      int returnAmnt;
+      
+      if(gameResult)
+      {
+         System.out.println(player.getFirstName() + " " + player.getLastName() + ", that is correct! You won " + WIN_AMNT);
+         returnAmnt = (int)Math.round(WIN_AMNT);
+      }
+      else
+      {
+         System.out.println(player.getFirstName() + " " + player.getLastName() + ", sorry, that is incorrect! You lost " + LOSE_AMNT);
+         returnAmnt = (int)Math.round(-LOSE_AMNT);
+      }
+      
+      return returnAmnt;
+   }
+}
